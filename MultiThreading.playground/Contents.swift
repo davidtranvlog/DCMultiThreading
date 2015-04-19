0c5255660bef49eac4cb03c159202579f68379cc
Developer Inspirus <duc@developerinspirus.com>
2015-04-19 12:16:18 -0400

    Intro to iOS Networking and MultiThreading
    Intro to iOS Networking and MultiThreading

---
+++ MultiThreading.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let API_KEY = "5e75c8d1a40e0df90862a24eb2325060"
+let NYLatitude = 40.75921100
+let NYLongitude = -73.98463800
+
+let stringURL = "https://api.forecast.io/forecast/5e75c8d1a40e0df90862a24eb2325060/\(NYLatitude),\(NYLongitude)"
+
+
+
+
+
+
+
+
+
+
+
